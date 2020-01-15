3e08d2403c12ac68c0386e8a04bfd830f2da080c
Patrick Santos <patrickspteixeira@gmail.com>
2020-01-15 09:09:57 -0300

    adiciona cadastro de usuarios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from 'App.module.css';
 import './Sidebar.css';
 import './Main.css';
+import api from 'services/api';
 
 function App() {
-  function handleSubmit(event) {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [github_username, setGithub] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      error => {
+        console.log('error:', error);
+      },
+      {
+        timeout: 30000,
+      },
+    );
+  }, []);
+
+  async function handleSubmit(event) {
     event.preventDefault();
+
+    const values = {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    };
+
+    const response = await api.post('devs', values);
+
+    setGithub('');
+    setTechs('');
   }
 
   return (
@@ -17,23 +51,47 @@ function App() {
           <div className={styles.block}>
             <label className="input" htmlFor="github_username">
               <span>Usuário do Github</span>
-              <input name="github_username" id="github_username" type="text" />
+              <input
+                name="github_username"
+                id="github_username"
+                type="text"
+                onChange={event => setGithub(event.target.value)}
+                value={github_username}
+              />
             </label>
 
             <label className="input" htmlFor="techs">
               <span>Tecnologias</span>
-              <input name="techs" id="techs" type="text" />
+              <input
+                name="techs"
+                id="techs"
+                type="text"
+                onChange={event => setTechs(event.target.value)}
+                value={techs}
+              />
             </label>
 
             <div className={styles.inline}>
               <label className="input" htmlFor="latitude">
                 <span>Latitude</span>
-                <input name="latitude" id="latitude" type="text" />
+                <input
+                  name="latitude"
+                  id="latitude"
+                  type="number"
+                  onChange={event => setLatitude(event.target.value)}
+                  value={latitude}
+                />
               </label>
 
               <label className="input" htmlFor="longitude">
                 <span>Longitude</span>
-                <input name="longitude" id="longitude" type="text" />
+                <input
+                  name="longitude"
+                  id="longitude"
+                  type="number"
+                  onChange={event => setLongitude(event.target.value)}
+                  value={longitude}
+                />
               </label>
             </div>
           </div>
